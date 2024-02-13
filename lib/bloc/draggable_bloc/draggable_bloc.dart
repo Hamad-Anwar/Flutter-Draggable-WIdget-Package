a76221d24064a9ae2c2b6a5414e42df4ad7f93a4
Hamad-Anwar <rh676838@gmail.com>
2024-02-13 12:19:06 +0500

    build

---
+++ lib/bloc/draggable_bloc/draggable_bloc.dart
@@ -0,0 +1,104 @@
+import 'package:bloc/bloc.dart';
+import 'package:draggable_widget/bloc/draggable_bloc/draggable_event.dart';
+import 'package:draggable_widget/bloc/draggable_bloc/draggable_state.dart';
+import 'package:flutter/material.dart';
+
+import '../../draggable_widget.dart';
+/// DraggableBloc responsible for managing the state of the draggable widget.
+class DraggableBloc extends Bloc<DraggableEvent, DraggableState> {
+  final InitialPosition initialPosition;
+  final Widget widget;
+  final double height;
+  final double width;
+  final double? left;
+  final double? top;
+  final BuildContext context;
+
+  /// Constructor for DraggableBloc.
+  ///
+  /// Parameters:
+  ///  - initialPosition: the initial position of the draggable widget.
+  ///  - context: the build context.
+  ///  - widget: the draggable widget.
+  ///  - height: the height of the draggable widget.
+  ///  - width: the width of the draggable widget.
+  ///  - left: optional left position for custom initial position.
+  ///  - top: optional top position for custom initial position.
+  DraggableBloc({
+    required this.initialPosition,
+    required this.context,
+    required this.widget,
+    required this.height,
+    required this.width,
+    this.left,
+    this.top,
+  }) : super(const DraggableState()) {
+    _initializeEventListeners();
+    add(InitialPositionEvent());
+  }
+
+  /// Initializes event listeners.
+  void _initializeEventListeners() {
+    on<InitialPositionEvent>(_setInitialPosition);
+    on<DragEvent>(_onDragEvent);
+  }
+
+
+  /// Updates the draggable widget's position on drag events.
+  void _onDragEvent(DragEvent event, Emitter<DraggableState> emit) {
+    emit(state.copyWith(top: event.top, left: event.left));
+  }
+  /// Sets the initial position of the draggable widget based on the chosen initial position.
+  void _setInitialPosition(InitialPositionEvent event, Emitter<DraggableState> emit) {
+    final Size screenSize = MediaQuery.of(context).size;
+    double left = 0;
+    double top = 0;
+    switch (initialPosition) {
+      case InitialPosition.center:
+        left = (screenSize.width - width) / 2;
+        top = (screenSize.height - height) / 2;
+        break;
+      case InitialPosition.topCenter:
+        left = (screenSize.width - width) / 2;
+        top = 0;
+        break;
+      case InitialPosition.bottomCenter:
+        left = (screenSize.width - width) / 2;
+        top = screenSize.height - height;
+        break;
+      case InitialPosition.leftCenter:
+        left = 0;
+        top = (screenSize.height - height) / 2;
+        break;
+      case InitialPosition.rightCenter:
+        left = screenSize.width - width;
+        top = (screenSize.height - height) / 2;
+        break;
+      case InitialPosition.topRightCorner:
+        left = screenSize.width - width;
+        top = 0;
+        break;
+      case InitialPosition.bottomRightCorner:
+        left = screenSize.width - width;
+        top = screenSize.height - height;
+        break;
+      case InitialPosition.topLeftCorner:
+        left = 0;
+        top = 0;
+        break;
+      case InitialPosition.bottomLeftCorner:
+        left = 0;
+        top = screenSize.height - height;
+        break;
+      case InitialPosition.custom:
+        left = this.left!;
+        top = this.top!;
+        break;
+      default:
+        break;
+    }
+
+    emit(state.copyWith(top: top, left: left));
+  }
+}
+
---
+++ lib/draggable_widget.dart
@@ -0,0 +1,141 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/draggable_bloc/draggable_bloc.dart';
+import 'bloc/draggable_bloc/draggable_event.dart';
+import 'bloc/draggable_bloc/draggable_state.dart';
+
+
+class DraggableWidget extends StatelessWidget {
+  final Widget widget;
+  final double height;
+  final double width;
+  final double dragSpeed;
+  final InitialPosition initialPosition;
+  final double? left;
+  final double? top;
+
+  /// DraggableWidget constructor.
+  /// Parameters:
+  ///  - widget: the widget to be draggable.
+  ///  - height: the height of the draggable widget.
+  ///  - width: the width of the draggable widget.
+  ///  - dragSpeed: the speed of dragging.
+  ///  - initialPosition: the initial position of the draggable widget.
+  ///  - left: optional left position for custom initial position.
+  ///  - top: optional top position for custom initial position.
+  DraggableWidget(
+      {Key? key,
+      required this.widget,
+      required this.initialPosition,
+      required this.height,
+      required this.width,
+      this.dragSpeed = 2.0,
+      this.left,
+      this.top,})
+      : super(key: key) {
+    _validateInput();
+  }
+
+  /// Validates input parameters.
+  void _validateInput() {
+    if (dragSpeed <= 0) {
+      throw Exception("Drag speed must be greater than 0");
+    }
+    if (initialPosition == InitialPosition.custom) {
+      if (left == null) {
+        throw Exception(
+            "You have to set left position with custom initial position");
+      }
+      if (top == null) {
+        throw Exception(
+            "You have to set top position with custom initial position");
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => DraggableBloc(
+        context: context,
+        height: height,
+        initialPosition: initialPosition,
+        widget: widget,
+        width: width,
+        left: left,
+        top: top,
+      ),
+      child: Builder(
+        builder: (context) {
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              return Overlay(
+                initialEntries: [
+                  OverlayEntry(
+                    builder: (context) {
+                      return BlocBuilder<DraggableBloc, DraggableState>(
+                        builder: (context, state) {
+                          return _buildPositioned(context, constraints, state);
+                        },
+                      );
+                    },
+                  )
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  /// Builds the Positioned widget.
+  Widget _buildPositioned(
+      BuildContext context, BoxConstraints constraints, DraggableState state) {
+    return Positioned(
+      /// Set position of the widget with the constrains
+      top: _getVerticalPosition(state.top, constraints.maxHeight),
+      left: _getBoundedPosition(state.left, constraints.maxWidth),
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          context.read<DraggableBloc>().add(
+                DragEvent(
+                  top: _getVerticalPosition(
+                      state.top + details.delta.dy * dragSpeed,
+                      constraints.maxHeight),
+                  left: _getBoundedPosition(
+                      state.left + details.delta.dx * dragSpeed,
+                      constraints.maxWidth),
+                ),
+              );
+        },
+        child: widget,
+      ),
+    );
+  }
+
+  /// Ensures the value stays within bounds (0 to maxValue - width).
+  double _getBoundedPosition(double value, double maxValue) {
+    return max(0, min(value, maxValue - width));
+  }
+
+  /// Ensures the value stays within bounds (0 to maxValue - height).
+  double _getVerticalPosition(double value, double maxValue) {
+    return max(0, min(value, maxValue - height));
+  }
+}
+/// Enum defining initial positions for the draggable widget.
+enum InitialPosition {
+  center,
+  topCenter,
+  leftCenter,
+  rightCenter,
+  bottomCenter,
+  topRightCorner,
+  bottomRightCorner,
+  topLeftCorner,
+  bottomLeftCorner,
+  custom
+}
